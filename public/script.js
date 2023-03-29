444e2b6a99fabd7429a869420c8694ef65a6895a
Badmus Eniola <115708246+somod-gif@users.noreply.github.com>
2023-03-29 12:22:09 +0100

    still working on bugs

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let screen = document.getElementById("screen")
+let screen = document.getElementById("screen");
 function dm() {
   if (document.body.style.backgroundColor == "white") {
     document.body.style.backgroundColor = "black";
@@ -17,13 +17,13 @@ let moviesarray = [
     description:
       " Queen Ramonda, Shuri, M'Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with Nakia and Everett Ross to forge a new path for their beloved kingdom.",
   },
-  // {
-  //   id: 2,
-  //   image: "./ride-film-2018-teaser-poster.jpg",
-  //   title: "The Rider",
-  //   description:
-  //     "The inspiring story of a BMX champion who overcame an abusive childhood through the love and life lessons of his interracial foster family.",
-  // },
+  {
+    id: 2,
+    image: "./ride-film-2018-teaser-poster.jpg",
+    title: "The Rider",
+    description:
+      "The inspiring story of a BMX champion who overcame an abusive childhood through the love and life lessons of his interracial foster family.",
+  },
   {
     id: 3,
     image: "./inheritance-2020-03.jpg",
@@ -147,6 +147,8 @@ let moviesarray = [
     description: "",
   },
 ];
+// let moviesarray = JSON.parse(localStorage.getItem("moviesarray"));
+// console.log(moviesarray);
 for (let index = 0; index < moviesarray.length; index++) {
   const element = moviesarray[index];
   document.getElementById("screen").innerHTML += `
@@ -155,20 +157,17 @@ for (let index = 0; index < moviesarray.length; index++) {
         <div class="mv-img mx-4 mt-2 ">
           <img src="${element.image}" alt="">  
         </div>
-        <h1>${element.title}</h1>
-        <button class="btn btn-outline-danger mb-3 mx-2" onclick="moviedetials(${index})"> Click to see full details</button>
+        <h4 class="mx-4">${element.title}</h4>
+        <button class="btn btn-outline-danger mx-4" onclick="moviedetials(${index})"><a class="cvc" href="#dtss">Click to see full details</a></button>
       </div>
     </div>
 `;
 }
-// console.log(moviesarray);
-
-// console.log(element);
-
+// let moviesarray = JSON.parsh(localStorage.getItem("moviesarray"));
 function moviedetials(index) {
   document.getElementById("movie_details").innerHTML = "";
   document.getElementById("movie_details").innerHTML += `
-  <div class="dtss">
+  <div id="dtss">
       <div>
           <div class="det-img">
               <img src="${moviesarray[index].image}" alt="">
@@ -177,42 +176,196 @@ function moviedetials(index) {
           <h3 class="text-light h3dts">${moviesarray[index].description}</h3>
       </div>
       <div class="d-flex">
-          <button class="btn btn-danger mx-4 mb-2">WATCH TRAILER</button>
-          <button class="btn btn-danger mx-4 mb-2">GET TICKET</button>
+      <button type="button" class="btn btn-danger mx-4 mb-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          WATCH TRAILER
+      </button>
+      <div class="modal fade modal-lg" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Watch Trailer for ${moviesarray[index].title}</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                      <div class="mx-5">
+                        <iframe width="660" height="340" src="https://www.youtube.com/embed/_Z3QKkl1WyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+                        </iframe>
+                      </div>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <button type="button" class="btn btn-danger mx-4 mb-2" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+          Book Ticket
+      </button>
+      <div class="modal fade modal-xl" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Make Payment for ${moviesarray[index].title} </h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                      <div class="aboutticket d-flex">
+                          <section class="ticket ">
+                              <h2>Ticket</h2>
+                              <p>Price: $100 per person</p>
+                              <div class="quantity">
+                                <button class="btn btn-outline-danger" id="decreasePersonsBtn" onclick="decreaseNumPersons()">-</button>
+                                <button class="btn btn-outline-danger" id="increasePersonsBtn" onclick="increaseNumPersons()">+</button>
+                                <h3>Number of Persons: <br> <span id="numPersons">1</span></h3>
+                              </div>
+                              <h3>Total:  $<span id="ticketAmount">100</span></h3>
+                          </section>
+                          <section class="refreshment mx-4 ">
+                              <h2>Refreshment</h2>
+                              <p>Price: $5 per popcorn and drink</p>
+                              <div class="quantity">
+                                  <button class="btn btn-outline-danger" id="decreaseRefreshmentBtn" onclick="removeRefreshment()">-</button>
+                                  <button  class="btn btn-outline-danger" id="increaseRefreshmentBtn" onclick="addRefreshment()">+</button>
+                                  <h4>Number of Refreshment for Each Person: <br> <span id="numRefreshments">0</span></h4>
+                              </div>
+                              <h3>Total: $<span id="refreshmentAmount">0</span></h3>
+                          </section>
+                        <section class="checkout">
+                            <h2>Checkout</h2>
+                            <button class="btn btn-outline-primary" id="checkoutBtn" onclick="makePayment()">Checkout</button>
+                            <h3>Total Amount: $<span id="totalAmount">100</span></h3>
+                        </section>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+        
       </div>
   </div>
   `;
-
 }
 // index();
 
-
-
 //  <img class="det-img" src="${moviesarray[index].image}" alt="">
-
 
 // Queen Ramonda, Shuri, M'Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with Nakia and Everett Ross to forge a new path for their beloved kingdom.
 
+{
+  /* <button type="button" class="btn btn-danger mx-4 mb-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  WATCH TRAILER
+</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/_Z3QKkl1WyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div> */
+}
+function makePayment() {
+  let ticketAmount = ticketPrice * numPersons;
+  let refreshmentAmount = refreshmentPrice * numRefreshments * numPersons;
+  let totalAmount = ticketAmount + refreshmentAmount;
+  FlutterwaveCheckout({
+    public_key: "FLWPUBK_TEST-52df64732ffd5c7c412d93d3aaf42cb6-X",
+    tx_ref: "titanic-48981487343MDI0NzMx",
+    amount: totalAmount,
+    currency: "USD",
+    payment_options: "card, mobilemoneyghana, ussd",
+    redirect_url: "https://glaciers.titanic.com/handle-flutterwave-payment",
+    meta: {
+      consumer_id: 23,
+      consumer_mac: "92a3-912ba-1192a",
+    },
+    customer: {
+      email: "somodbadmus@gmail.com",
+      phone_number: "08146438621",
+      name: "Badmus Eniola",
+    },
+    customizations: {
+      title: "The Genesis Cinema",
+      description:
+        "Enjoy unlimited fun and total comfort watching movies at our cinemas with friends and loved ones🥳",
+      logo: "https://genesiscinemas.com/wp-content/uploads/2022/04/LOGO.png",
+    },
+  });
+  window.print();
+}
+function gotosignin() {
+  window.location.href = "./signup.html";
+}
+// initial values
+let ticketPrice = 100;
+let refreshmentPrice = 5;
+let numPersons = 1;
+let numRefreshments = 0;
 
+// functions for updating the amounts
+function updateTicketAmount() {
+  let ticketAmount = ticketPrice * numPersons;
+  document.getElementById("ticketAmount").innerHTML = ticketAmount;
+  updateTotalAmount();
+}
 
+function addRefreshment() {
+  numRefreshments += 1;
+  document.getElementById("numRefreshments").innerHTML = numRefreshments;
+  updateRefreshmentAmount();
+}
 
+function removeRefreshment() {
+  if (numRefreshments > 0) {
+    numRefreshments -= 1;
+    document.getElementById("numRefreshments").innerHTML = numRefreshments;
+    updateRefreshmentAmount();
+  }
+}
 
+function updateRefreshmentAmount() {
+  let refreshmentAmount = refreshmentPrice * numRefreshments * numPersons;
+  document.getElementById("refreshmentAmount").innerHTML = refreshmentAmount;
+  updateTotalAmount();
+}
 
+function increaseNumPersons() {
+  numPersons += 1;
+  document.getElementById("numPersons").innerHTML = numPersons;
+  updateTicketAmount();
+}
 
+function decreaseNumPersons() {
+  if (numPersons > 1) {
+    numPersons -= 1;
+    document.getElementById("numPersons").innerHTML = numPersons;
+    updateTicketAmount();
+  }
+}
 
+function updateTotalAmount() {
+  let ticketAmount = ticketPrice * numPersons;
+  let refreshmentAmount = refreshmentPrice * numRefreshments * numPersons;
+  let totalAmount = ticketAmount + refreshmentAmount;
+  document.getElementById("totalAmount").innerHTML = totalAmount;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+// initial updates
+updateTicketAmount();
+updateRefreshmentAmount();
+updateTotalAmount();
 //npm init react-app reactjs
 //https://apply.workable.com/kuda/
 //https://web.whatsapp.com/desktop/windows/release/x64/WhatsAppSetup.exe
@@ -220,31 +373,3 @@ function moviedetials(index) {
 // PYTHON
 // MSQL
 // DJANGO
-
-
-
-
-// function makePayment() {
-//   FlutterwaveCheckout({
-//     public_key: "FLWPUBK_TEST-4765698c71c539554658c7ab44c26174-X",
-//     tx_ref: "titanic-48981487343MDI0NzMx",
-//     amount: 2500,
-//     currency: "NGN",
-//     payment_options: "card, mobilemoneyghana, ussd",
-//     redirect_url: "https://glaciers.titanic.com/handle-flutterwave-payment",
-//     meta: {
-//       consumer_id: 23,
-//       consumer_mac: "92a3-912ba-1192a",
-//     },
-//     customer: {
-//       email: "rose@unsinkableship.com",
-//       phone_number: "08102909304",
-//       name: "Rose DeWitt Bukater",
-//     },
-//     customizations: {
-//       title: "The Titanic Store",
-//       description: "Payment for an awesome cruise",
-//       logo: "https://www.logolynx.com/images/logolynx/22/2239ca38f5505fbfce7e55bbc0604386.jpeg",
-//     },
-//   });
-// }
